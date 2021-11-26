2bc0622a5a1412aca20bb1e4294cfeb0e894f149
Pitipat <pitipat_fu@cmu.ac.th>
2021-11-26 16:45:09 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int x , y = 0 , z = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x ;
+    if ( x % 2 == 0){
+            y = y+1 ;
+        }else {
+            z = z+1 ;
+        }
+    while ( x != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> x ;
+
+        if ( x % 2 == 0){
+            y = y+1 ;
+        }else {
+            z = z+1 ;
+        }
+    }
+    cout << "#Even numbers = " << y-1 << "\n" ;
+    cout << "#Odd numbers = " << z;
     return 0;
 }
