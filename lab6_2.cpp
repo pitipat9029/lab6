7e656cfdeb8c3eb47420372d33c56238ebfcda0c
Pitipat <pitipat_fu@cmu.ac.th>
2021-11-26 17:22:30 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
 
+double deg2rad(double x ){
+    double d = x * M_PI / 180 ;
+    return d ;
+}
+
+double rad2deg(double y){
+    double r = y * 180 / M_PI ;
+    return r ;
+}
+
+double findXComponent(double L1 , double L2 , double A1 , double A2 ){
+    double L = L1*cos(A1) + L2*cos(A2) ;
+    return L ;
+}
+
+double findYComponent(double L1 , double L2 , double A1 , double A2 ){
+    double L = L1*sin(A1) + L2*sin(A2) ;
+    return L ;
+}
+
+double pythagoras(double XC , double YC){
+    double P = sqrt(pow(XC,2)+pow(YC,2));
+    return P ;
+} 
+
+void showResult(double result_vec_length , double result_vec_direction ){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << result_vec_length << "\n";
+    cout << "Direction of the resultant vector (deg) = " << result_vec_direction << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}  
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
